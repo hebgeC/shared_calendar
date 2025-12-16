7e0e0487e83dd5d9ae2c49bfd8fe280069d21af4
cchas <cchase4210@gmail.com>
2025-12-16 00:48:25 -0800

    Refactor CalendarEvent to use DateTime for event times
    Simplified the CalendarEvent class by replacing separate DateTime and TimeOnly fields (with string properties and custom parsing logic) for StartTime and EndTime with straightforward DateTime properties. Updated the constructor to initialize these properties to DateTime.Now, improving code clarity and maintainability.

--- shared_calendar/Models/CalendarEvent.cs
+++ shared_calendar/Models/CalendarEvent.cs
@@ -6,72 +6,16 @@ namespace shared_calendar.Models
     {
         public CalendarEvent() 
         {
-            this.date = DateTime.Now;
-            this.endTime = TimeOnly.FromDateTime(DateTime.Now);
-            this.startTime = TimeOnly.FromDateTime(DateTime.Now);
+            this.StartTime = DateTime.Now;
+            this.EndTime = DateTime.Now;
             this.Title = string.Empty;
             this.Description = string.Empty;
         }
 
-        private TimeOnly startTime;
-        private TimeOnly endTime;
-        private DateTime date;
-
         public int id { get; set; }
-
-        public DateTime? Date 
-        {
-            get
-            {
-                return this.date;
-            }
-            set
-            {
-                this.date = (DateTime)value;
-            }
-        }
-
-        public string StartTime 
-        {
-            get
-            {
-                return this.startTime.ToString();
-            }
-
-            set
-            {
-                try
-                {
-                    startTime = TimeOnly.Parse(value);
-                } 
-                catch (Exception e)
-                {
-                    Console.WriteLine(e);
-                    this.startTime = new TimeOnly();
-                }
-            }
-        } 
-        public string EndTime
-        {
-            get
-            {
-                return this.endTime.ToString();
-            }
-            set
-            {
-                try
-                {
-                    endTime = TimeOnly.Parse(value);
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e);
-                    this.endTime = new TimeOnly();
-                }
-            }
-        }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
     }
 }
-
