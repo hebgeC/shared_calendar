d5af06c0049ee755355564f6912a689024018844
cchas <cchase4210@gmail.com>
2025-12-19 02:06:24 -0800

    Refactor event model to use DateTime fields
    Condense event date and time into StartDateTime and EndDateTime fields in the database. Update migration, model snapshot, and UI components to reflect this change. Refactor DateTimePicker and related forms for improved consistency and streamlined data handling. Minor code style improvements in TimePicker.

--- shared_calendar/Data/Migrations/20251219012036_timesAndDateCondenced.Designer.cs
+++ shared_calendar/Data/Migrations/20251219012036_timesAndDateCondenced.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using shared_calendar.Data;
 
@@ -11,9 +12,11 @@ using shared_calendar.Data;
 namespace shared_calendar.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251219012036_timesAndDateCondenced")]
+    partial class timesAndDateCondenced
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ shared_calendar/Data/Migrations/20251219012036_timesAndDateCondenced.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace shared_calendar.Migrations
+{
+    /// <inheritdoc />
+    public partial class timesAndDateCondenced : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EndTime",
+                table: "CalendarEvent");
+
+            migrationBuilder.DropColumn(
+                name: "StartTime",
+                table: "CalendarEvent");
+
+            migrationBuilder.RenameColumn(
+                name: "Date",
+                table: "CalendarEvent",
+                newName: "StartDateTime");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EndDateTime",
+                table: "CalendarEvent",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EndDateTime",
+                table: "CalendarEvent");
+
+            migrationBuilder.RenameColumn(
+                name: "StartDateTime",
+                table: "CalendarEvent",
+                newName: "Date");
+
+            migrationBuilder.AddColumn<string>(
+                name: "EndTime",
+                table: "CalendarEvent",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "StartTime",
+                table: "CalendarEvent",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
